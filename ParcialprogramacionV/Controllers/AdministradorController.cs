6d1d12f65cdf89525d49481307643a3096db0142
Usuario <Usuario@DESKTOP-O8V1MOG>
2024-06-01 20:20:10 -0300

    Agregar archivos de proyecto.

---
+++ ParcialprogramacionV/Controllers/AdministradorController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using ParcialprogramacionV.Datos;
+using ParcialprogramacionV.Models;
+
+namespace ParcialprogramacionV.Controllers
+{
+    public class AdministradorController : Controller
+    {
+
+         UsuarioDatos _UsuarioDatos = new UsuarioDatos();
+
+        public IActionResult Listar()
+        {
+            //LA VISTA MOSTRARÁ UNA LISTA DE CONTACTOS
+            var oLista = _UsuarioDatos.Listar();
+
+            return View(oLista);
+        }
+
+        public IActionResult Guardar()
+        {
+            //METODO SOLO DEVUELVE LA VISTA
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Guardar(UsuarioModel oUsuario)
+        {
+            //METODO RECIBE EL OBJETO PARA GUARDARLO EN BD
+            if (!ModelState.IsValid)
+                return View();
+
+
+            var respuesta = _UsuarioDatos.Guardar(oUsuario);
+
+            if (respuesta)
+                return RedirectToAction("Listar");
+            else
+                return View();
+        }
+
+        public IActionResult Editar(int IdUsuario)
+        {
+            //METODO SOLO DEVUELVE LA VISTA
+            var ousuario = _UsuarioDatos.Obtener(IdUsuario);
+            return View(ousuario);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(UsuarioModel oUsuario)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            var respuesta = _UsuarioDatos.Editar(oUsuario);
+            if (respuesta)
+                return RedirectToAction("Listar");
+            else
+                return View();
+        }
+
+
+        public IActionResult Eliminar(int IdUsuario)
+        {
+            //METODO SOLO DEVUELVE LA VISTA
+            var ousuario = _UsuarioDatos.Obtener(IdUsuario);
+            return View(ousuario);
+        }
+
+        [HttpPost]
+        public IActionResult Eliminar(UsuarioModel oUsuario)
+        {
+            var respuesta = _UsuarioDatos.Eliminar(oUsuario.IdUsuario);
+            if (respuesta)
+                return RedirectToAction("Listar");
+            else
+                return View();
+        }
+
+    }
+}
---
+++ ParcialprogramacionV/Models/UsuarioModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParcialprogramacionV.Models
+{
+    public class UsuarioModel
+    {
+        public int IdUsuario { get; set; }
+
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
+        public string? Nombre { get; set; }
+        [Required(ErrorMessage = "El campo Apellido es obligatorio")]
+        public string? Apellido { get; set; }
+        [Required(ErrorMessage = "El campo Dni es obligatorio")]
+        public string? Dni { get; set; }
+        [Required(ErrorMessage = "El campo Telefono es obligatorio")]
+        public string? Telefono { get; set; }
+        [Required(ErrorMessage = "El campo Correo es obligatorio")]
+        public string? Correo { get; set; }
+
+    }
+}
